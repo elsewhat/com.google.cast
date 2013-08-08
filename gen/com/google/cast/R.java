a5adeea5df7af3f4fc6d66f2b83a60c7ef1882b3
Dagfinn Parnas <dagfinn.parnas@gmail.com>
2013-08-08 22:54:39 +0200

    Revert "dummy commit"
    This reverts commit 3385009453533d693ff4028019b826234047f68c.

--- gen/com/google/cast/R.java
+++ gen/com/google/cast/R.java
@@ -11,19 +11,19 @@ public final class R {
     public static final class attr {
     }
     public static final class string {
-        public static int error_no_session=0x7f020003;
+        public static final int error_no_session=0x7f020003;
         /**  Error message from Cast MediaRouteProvider that indicates that the provider got an
          error when sending a media command (e.g., load media, pause, etc.) to the selected
          Cast device. [CHAR LIMIT=NONE] 
          */
-        public static int error_ramp_command_failed=0x7f020002;
+        public static final int error_ramp_command_failed=0x7f020002;
         /**  Error message from Cast MediaRouteProvider that indicates that the provider has lost
          its connection to the selected Cast device. [CHAR LIMIT=NONE] 
          */
-        public static int error_session_ended=0x7f020001;
+        public static final int error_session_ended=0x7f020001;
         /**  Error message from Cast MediaRouteProvider that indicates that the provider was not
          able to connect to the selected Cast device. [CHAR LIMIT=NONE] 
          */
-        public static int error_start_session_failed=0x7f020000;
+        public static final int error_start_session_failed=0x7f020000;
     }
 }
